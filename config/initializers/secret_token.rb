33bdb2540effebbe6daf89bcef459fdf1375497f
petemcfarlane <peterjohnmcfarlane@gmail.com>
2013-07-12 15:17:19 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hubly::Application.config.secret_key_base = 'e3e49aa0fa6794efe0de3530b43fd7fb22a596428e72ac30af0f51e12f8f6029788df2d6370a3b62b56ce3518a37ed5def02d0e462737fd9070fbd11da691281'
