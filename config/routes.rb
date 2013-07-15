266af05084a32fcf7185cb4c8bb16f2a33b1c0fd
petemcfarlane <peterjohnmcfarlane@gmail.com>
2013-07-15 11:42:58 +0100

    added staticpages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Hubly::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  #root 'static_pages#index'
+  get 'home' => 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
